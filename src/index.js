07a88ee709d3b8698110fb9a560f6f555b88e75b
kylie <tinkerchelle93@yahoo.com>
2018-10-28 20:12:49 -0400

    did stuff

--- src/index.js
+++ src/index.js
@@ -18,13 +18,13 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload() {
   game.load.image('logo', './assets/images/phaser.png');
 }
+var logo;
 
 function create() {
-  var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-  logo.anchor.setTo(0.5, 0.5);
+  logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+  //logo.anchor.setTo(0.5, 0.5);
 };
 
 function update() {
-  // ¯ \_(ツ)_/¯
-  // "surprise me"
+  logo.x--;
 }
